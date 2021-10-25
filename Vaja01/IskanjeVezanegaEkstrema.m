fd68465d4b429b87fed08ab6ef4d148a9e590d99
Luka Toplak <luka.toplak@student.fmf.uni-lj.si>
2021-10-25 13:49:02 +0200

    vaja3 started

--- Vaja01/IskanjeVezanegaEkstrema.m
+++ Vaja01/IskanjeVezanegaEkstrema.m
@@ -4,11 +4,18 @@ f = @(x,y) exp(x.^2 - y.^2);
 interval = linspace(-1,1);
 [X,Y] = meshgrid(interval,interval);
 
-contour(X,Y,f(X,Y));
 hold on;
+daspect([1 1]);
+contour(X,Y,f(X,Y));
 
-p = @(x,y) (x .- 1/3).^2 + (y .- 1/3).^2 - 1/3;
+p = @(x,y) (x - 1/3).^2 + (y - 1/3).^2 - 1/3;
 ezplot(p,[-1 1 -1 1]); % fimplicit ni v Octave
 hold off;
 
-fmincon(f,[.3 .3],[],[],[],[])
+fmincon(f,[.6;.6;],[],[],[],[],[-.3 -.3],[1 1],@circleCon);
+
+
+function [cneq,ceq] = circleCon(x)
+	cneq = [];
+	ceq = (x(1) - 1/3).^2 + (x(2) - 1/3).^2 - 1/3;
+end
