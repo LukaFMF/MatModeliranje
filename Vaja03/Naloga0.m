c7a87ad91848227383907172edef9de1b8c04a3e
LukaFMF <luka.toplak@student.fmf.uni-lj.si>
2021-10-27 02:38:46 +0200

    Vaja03 started

--- Vaja03/Naloga0.m
+++ Vaja03/Naloga0.m
@@ -1,29 +1,31 @@
-1;
+tiledlayout(2,2);
+
 % a)
 yPrime = @(t,y) 2*t;
-[t,y] = ode45(yPrime,[0 5],1)
+[t,y] = ode45(yPrime,[0 5],1);
 
 tocnaResitev = @(t) t.^2 + 1;
 yTocnaResitev = tocnaResitev(t);
 
-figure 1;
+
+nexttile;
 plot(t,y,"r");
 plot(t,yTocnaResitev,"g");
 
-figure 2;
+nexttile;
 plot(t,abs(y - yTocnaResitev));
 
 % b)
 y2Prime = @(t,y) 6*t;
 F = @(t,Y) [Y(2); y2Prime(t,Y(2));];
-[t,y] = ode45(F,[0 5],[1 0])
+[t,y] = ode45(F,[0 5],[1 0]);
 
 tocnaResitev = @(t) t.^3 + 1;
 yTocnaResitev = tocnaResitev(t);
 
-figure 3;
+nexttile;
 plot(t,y,"r");
 plot(t,yTocnaResitev,"g");
 
-figure 4;
-plot(t,abs(y - yTocnaResitev));
+nexttile;
+plot(t,abs(y(:,1) - yTocnaResitev));
