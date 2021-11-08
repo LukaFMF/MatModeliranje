6e47e5c4690c9239f92d71342a44e6fb3c5ad392
Luka Toplak <luka.toplak@student.fmf.uni-lj.si>
2021-11-08 14:03:16 +0100

    resevanje Vaja3

--- Vaja03/Naloga1.m
+++ Vaja03/Naloga1.m
@@ -1,3 +1,21 @@
+m = 105; % kg; masa padalca
+c_u = 1; % /; koeficient zracnega upora (odvisen od oblike telesa)
+S = 1.2; % m^2; povrsina precnega preseka telesa 
+
+% 1)
+[y,v,t] = padalec([m c_u S],[40000 0],300,10000);
+aritmeticnaSredina = mean(v)
+
+plot(t,y,"r");
+
+% 2)
+p_z = 1.225;
+g = 9.81;
+% 0 = -g - (p_z*c_u*S)/(2*m)*y'*abs(y'); y' < 0
+% g = (p_z*c_u*S)/(2*m)*abs(y')^2
+% y'= - sqrt((2*m*g)/(p_z*c_u*S))
+najvecjaTeoreticaHItrost = - sqrt((2*m*g)/(p_z*c_u*S))
+
 function [y,v,t] = padalec(parametri,zac,tk,n)
 % function [y,v,t] = padalec(parametri,zac,tk,n)
 % 
@@ -17,6 +35,12 @@ function [y,v,t] = padalec(parametri,zac,tk,n)
 % v so hitrosti padalca ob casih t (vektor dolzine n)
 % t je vektor casovnih trenutkov
 
+t = linspace(0,tk,n);
+F = @(t,Y) diferencialniSistem(t,Y,parametri);
+[~,Y] = ode45(F,t,zac);
+
+y = Y(:,1);
+v = Y(:,2);
 end
 	
 	
@@ -34,4 +58,9 @@ function dY = diferencialniSistem(t,Y,parametri)
 % Izhod:
 % dY je sistem NDE, vrnemo desno stran sistema dY = F(t,Y)
 
+g_0 = 9.81; % m/s^2; teznostni pospesek na nadmorski vsisin 0 m
+p_z = 1.225; % kg/m^3; gostota zraka
+k = (p_z*parametri(2)*parametri(3))/(2*parametri(1));
+
+dY = [Y(2); - g_0 - k*abs(Y(2)).*Y(2)];
 end
